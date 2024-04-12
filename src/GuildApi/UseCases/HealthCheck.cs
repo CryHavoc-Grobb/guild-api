2dde79e42ddf72e21d531b12fd090798f0fcf313
Jeremy Stafford <jeremy.j.stafford@gmail.com>
2024-04-12 14:27:11 -0700

    chore: healthcheck endpoint

---
+++ src/GuildApi/UseCases/HealthCheck.cs
@@ -0,0 +1,15 @@
+﻿using Ardalis.ApiEndpoints;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GuildApi.UseCases;
+
+public class HealthCheck : EndpointBaseSync
+    .WithoutRequest
+    .WithActionResult
+{
+    [HttpGet("/")]
+    public override ActionResult Handle()
+    {
+        return Ok();
+    }
+}
